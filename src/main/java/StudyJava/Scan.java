936cb7194b37ed724bed9bf66a9049dfab0b9f51
하영 이 <gkdudp9@naver.com>
2022-01-16 17:54:17 +0900

    입력받은 문자열을 출력합니다.

---
+++ src/main/java/StudyJava/Scan.java
@@ -0,0 +1,14 @@
+package StudyJava;
+
+import org.w3c.dom.ls.LSInput;
+
+import java.util.Scanner;
+
+public class Scan {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+
+        System.out.println(str);
+    }
+}
